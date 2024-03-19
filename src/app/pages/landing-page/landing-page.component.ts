8204bc8c49e35c3012bf4820ed75b883957ee024
CaueFer <cauefernandescaetano@gmail.com>
2024-03-19 17:24:05 -0300

    create brans carousel

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -8,7 +8,7 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
 })
 export class LandingPageComponent {
 
-  carouselOptions: OwlOptions = {
+  mainOptions: OwlOptions = {
     loop: true,
     mouseDrag: false,
     touchDrag: false,
@@ -23,6 +23,31 @@ export class LandingPageComponent {
     },
     nav: false,
     autoplay: true,
-    autoplayTimeout: 1500,
+    autoplayTimeout: 3500,
+  }
+
+  brandsOption: OwlOptions = {
+    items: 1,
+    loop: true,
+    margin: 24,
+    nav: false,
+    dots: false,
+    stagePadding: 0,
+    responsive: {
+      672: {
+        items: 4
+      },
+      912: {
+        items: 5
+      },
+    },
+    autoplay: true,
+    slideTransition: 'linear',
+    autoplayTimeout: 0,
+    autoplaySpeed: 3000,
+    autoplayHoverPause: false,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
   }
 }
