6db1257c105062569362bffbd50586751f4a46f5
CaueFer <cauefernandescaetano@gmail.com>
2024-03-24 00:09:08 -0300

    add 3d car model & camera rotation

--- src/app/shared/micro-components/3d/cube-test/cube-test.component.ts
+++ src/app/shared/micro-components/3d/cube-test/cube-test.component.ts
@@ -1,5 +1,12 @@
 import { Component, NgZone, afterNextRender } from '@angular/core';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { Tween } from '@tweenjs/tween.js';
+
+
+// LOADERS
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { VOXLoader } from 'three/examples/jsm/loaders/VOXLoader';
 
 @Component({
   selector: 'app-cube-test',
@@ -10,39 +17,86 @@ export class CubeTestComponent {
 
   constructor(private window: Window){
     afterNextRender(() => {
-      const width = window.innerWidth, height = window.innerHeight;
+      const width = window.innerWidth;
+      const height = window.innerHeight;
 
-      // init-
-
-      const camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
-      camera.position.z = 1;
-
+      // RENDER
+      const renderer = new THREE.WebGLRenderer({ antialias: true });
+      renderer.setSize(width, height);
+      renderer.shadowMap.enabled = true;
+      document.body.appendChild(renderer.domElement);
+  
+      // CAMERA 
+      const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
+      camera.position.set(0, 1, 5);
+  
       const scene = new THREE.Scene();
 
-
-      // GEOMETRY OF CUBE
-      const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-      const material = new THREE.MeshNormalMaterial();
-      const mesh = new THREE.Mesh( geometry, material );
-      scene.add( mesh );
+      const axesHelper = new THREE.AxesHelper(5);
+      scene.add(axesHelper);
 
 
-      // RENDER (JUNCAO DE TUDO)
-      const renderer = new THREE.WebGLRenderer( { antialias: true } );
-      renderer.setSize( width, height );
-      renderer.setAnimationLoop( animation );
-      document.body.appendChild( renderer.domElement );
+      const orbitControls = new OrbitControls(camera, renderer.domElement);
+      orbitControls.update();
 
-      // animation
+      window.addEventListener('mouseup', () => {
+                // Posição inicial da câmera
+                const startPosition = { x: camera.position.x, y: camera.position.y, z: camera.position.z };
+                // Posição alvo da câmera (posição padrão)
+                const targetPosition = { x: 0, y: 1, z: 5 };
+        
+                // Cria uma nova animação Tween com Tween.js
+                new Tween(startPosition)
+                  .to(targetPosition, 1000) 
+                  .easing(TWEEN.Easing.Quadratic.Out)
+                  .onUpdate(() => {
+                    camera.position.set(startPosition.x, startPosition.y, startPosition.z);
+                    camera.lookAt(0, 1, 0); 
+                    orbitControls.update(); 
+                  })
+                  .start();
+      });
 
-      function animation( time: number ) {
 
-        mesh.rotation.x = time / 2000;
-        mesh.rotation.y = time / 1000;
+      // LUZ 
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+      directionalLight.position.set(0, 5, 0);
+      directionalLight.castShadow = true;
+      scene.add(directionalLight);
 
-        renderer.render( scene, camera );
+      const lightHelper = new THREE.DirectionalLightHelper(directionalLight);
+      scene.add(lightHelper);
 
-      }
+  
+      // CHAO
+      const groundGeometry = new THREE.PlaneGeometry(10, 10); // Ajuste o tamanho conforme necessário
+      const groundMaterial = new THREE.MeshStandardMaterial({ color: 0x888888 }); // Cor cinza
+      const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+      ground.rotation.x = -Math.PI / 2; // ROTACIONA 90 graus
+      ground.receiveShadow = true;
+      scene.add(ground);
+
+
+      // LOADER DO MODELO 3D
+      const gltfLoader = new GLTFLoader();
+      gltfLoader.load('assets/3d-assets/porsche-911/scene.gltf', (gltfScene) => {
+
+        gltfScene.scene.rotation.y = THREE.MathUtils.degToRad(-90);
+        gltfScene.scene.position.set(0, 0.65, 0);
+        gltfScene.scene.castShadow = true;
+
+        scene.add(gltfScene.scene);
+      });
+  
+      const animate = () => {
+
+        // ANIMAR AQUI
+
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+      };
+  
+      animate();
     });
   }
 } 
