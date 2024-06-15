e007b2c8b782a9486cfab7a424dec448c6543f47
CaueFer <cauefernandescaetano@gmail.com>
2024-06-15 18:02:18 -0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RouteGuard } from './core/services/route.guard';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
 const routes: Routes = [
   {path: "", loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)},
-
-  {path: "home", component: LandingPageComponent},
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: '', pathMatch: 'full' },
   {path: "account", loadChildren: () => import('./account/account.module').then(m => m.AccountModule)},
-
 ];
 
 @NgModule({
